4dbe9a0917aabc9f89b717a5ee0d768a857af45c
letyang23 <yangletian0309@gmail.com>
2023-04-30 22:26:22 -0700

    Phase 1

--- apps/queue_tester_example.c
+++ apps/queue_tester_example.c
@@ -4,43 +4,123 @@
 
 #include <queue.h>
 
-#define TEST_ASSERT(assert)				\
-do {									\
-	printf("ASSERT: " #assert " ... ");	\
-	if (assert) {						\
-		printf("PASS\n");				\
-	} else	{							\
-		printf("FAIL\n");				\
-		exit(1);						\
-	}									\
+#define TEST_ASSERT(assert)                \
+do {                                    \
+    printf("ASSERT: " #assert " ... ");    \
+    if (assert) {                        \
+        printf("PASS\n");                \
+    } else    {                            \
+        printf("FAIL\n");                \
+        exit(1);                        \
+    }                                    \
 } while(0)
 
 /* Create */
-void test_create(void)
-{
-	fprintf(stderr, "*** TEST create ***\n");
+void test_create(void) {
+    fprintf(stderr, "*** TEST create ***\n");
 
-	TEST_ASSERT(queue_create() != NULL);
+    TEST_ASSERT(queue_create() != NULL);
 }
 
 /* Enqueue/Dequeue simple */
-void test_queue_simple(void)
-{
-	int data = 3, *ptr;
-	queue_t q;
+void test_queue_simple(void) {
+    int data = 3, *ptr;
+    queue_t q;
 
-	fprintf(stderr, "*** TEST queue_simple ***\n");
+    fprintf(stderr, "*** TEST queue_simple ***\n");
 
-	q = queue_create();
-	queue_enqueue(q, &data);
-	queue_dequeue(q, (void**)&ptr);
-	TEST_ASSERT(ptr == &data);
+    q = queue_create();
+    queue_enqueue(q, &data);
+    queue_dequeue(q, (void **) &ptr);
+    TEST_ASSERT(ptr == &data);
 }
 
-int main(void)
-{
-	test_create();
-	test_queue_simple();
+/* Enqueue/Dequeue multiple */
+void test_queue_multiple(void) {
+    int data1 = 1, data2 = 2, *ptr;
+    queue_t q;
 
-	return 0;
+    fprintf(stderr, "*** TEST queue_multiple ***\n");
+
+    q = queue_create();
+    queue_enqueue(q, &data1);
+    queue_enqueue(q, &data2);
+    TEST_ASSERT(queue_length(q) == 2);
+
+    queue_dequeue(q, (void **) &ptr);
+    TEST_ASSERT(ptr == &data1);
+    TEST_ASSERT(queue_length(q) == 1);
+
+    queue_dequeue(q, (void **) &ptr);
+    TEST_ASSERT(ptr == &data2);
+    TEST_ASSERT(queue_length(q) == 0);
+}
+
+/* Delete */
+void test_queue_delete(void) {
+    int data1 = 1, data2 = 2;
+    queue_t q;
+
+    fprintf(stderr, "*** TEST queue_delete ***\n");
+
+    q = queue_create();
+    queue_enqueue(q, &data1);
+    queue_enqueue(q, &data2);
+
+    TEST_ASSERT(queue_delete(q, &data1) == 0);
+    TEST_ASSERT(queue_length(q) == 1);
+}
+
+/* Iterate */
+void test_queue_iterate(void) {
+    int data1 = 1, data2 = 2;
+    queue_t q;
+
+    fprintf(stderr, "*** TEST queue_iterate ***\n");
+
+    q = queue_create();
+    queue_enqueue(q, &data1);
+    queue_enqueue(q, &data2);
+
+    queue_iterate(q, (queue_func_t) printf);
+
+    printf("\n");
+}
+
+
+static void iterator_inc(queue_t q, void *data) {
+    int *a = (int *) data;
+
+    if (*a == 42)
+        queue_delete(q, data);
+    else
+        *a += 1;
+}
+
+void test_iterator(void) {
+    queue_t q;
+    int data[] = {1, 2, 3, 4, 5, 42, 6, 7, 8, 9};
+    size_t i;
+
+    /* Initialize the queue and enqueue items */
+    q = queue_create();
+    for (i = 0; i < sizeof(data) / sizeof(data[0]); i++)
+        queue_enqueue(q, &data[i]);
+
+    /* Increment every item of the queue, delete item '42' */
+    queue_iterate(q, iterator_inc);
+    TEST_ASSERT(data[0] == 2);
+    TEST_ASSERT(queue_length(q) == 9);
+}
+
+
+int main(void) {
+    test_create();
+    test_queue_simple();
+    test_queue_multiple();
+    test_queue_delete();
+    test_queue_iterate();
+    test_iterator();
+
+    return 0;
 }
--- libuthread/queue.c
+++ libuthread/queue.c
@@ -4,42 +4,146 @@
 
 #include "queue.h"
 
+typedef struct node {
+    void *data;
+    struct node *next;
+} node_t;
+
 struct queue {
-	/* TODO Phase 1 */
+    int size;
+    node_t *head;
+    node_t *tail;
 };
 
-queue_t queue_create(void)
-{
-	/* TODO Phase 1 */
+queue_t queue_create(void) {
+    queue_t new_queue = (queue_t) malloc(sizeof(struct queue));
+
+    if (new_queue == NULL) {
+        return NULL;
+    }
+
+    new_queue->size = 0;
+    new_queue->head = NULL;
+    new_queue->tail = NULL;
+    return new_queue;
 }
 
-int queue_destroy(queue_t queue)
-{
-	/* TODO Phase 1 */
+int queue_destroy(queue_t queue) {
+    if (queue == NULL || queue->size != 0) {
+        return -1;
+    } else {
+        free(queue);
+        return 0;
+    }
 }
 
-int queue_enqueue(queue_t queue, void *data)
-{
-	/* TODO Phase 1 */
+int queue_enqueue(queue_t queue, void *data) {
+    if (queue == NULL || data == NULL) {
+        return -1;
+    }
+
+    node_t *new_node = (node_t *) malloc(sizeof(node_t));
+    new_node->data = data;
+    new_node->next = NULL;
+    if (new_node == NULL) {
+        return -1;
+    }
+
+    if (queue->size == 0) {
+        queue->head = new_node;
+        queue->tail = new_node;
+    } else {
+        queue->tail->next = new_node;
+        queue->tail = new_node;
+    }
+    ++queue->size;
+    return 0;
 }
 
-int queue_dequeue(queue_t queue, void **data)
-{
-	/* TODO Phase 1 */
+int queue_dequeue(queue_t queue, void **data) {
+    if (queue == NULL || data == NULL || queue->size == 0) {
+        return -1;
+    }
+    node_t *cur = queue->head;
+    *data = cur->data;
+    queue->head = cur->next;
+
+    if (queue->size == 1) {
+        queue->tail = NULL;
+    }
+    --queue->size;
+    free(cur);
+    return 0;
 }
 
-int queue_delete(queue_t queue, void *data)
-{
-	/* TODO Phase 1 */
+int queue_delete(queue_t queue, void *data) {
+    if (queue == NULL || data == NULL) {
+        return -1;
+    }
+
+    node_t *cur = queue->head;
+    node_t *prev = NULL;
+    int match = 0;
+
+    while (cur != NULL) {
+        if (cur->data == data) {
+            match = 1;
+            break;
+        } else {
+            prev = cur;
+            cur = cur->next;
+        }
+    }
+
+    if (match) {
+        if (prev == NULL) {
+            queue->head = cur->next;
+        } else {
+            prev->next = cur->next;
+        }
+
+        if (cur->next == NULL) {
+            queue->tail = prev;
+        }
+        free(cur);
+        --queue->size;
+        return 0;
+    }
+
+    return -1;
 }
 
-int queue_iterate(queue_t queue, queue_func_t func)
-{
-	/* TODO Phase 1 */
+int queue_iterate(queue_t queue, queue_func_t func) {
+    if (queue == NULL || func == NULL) {
+        return -1;
+    }
+
+    node_t *cur = queue->head;
+    node_t *prev = NULL;
+    node_t *next = NULL;
+
+    while (cur != NULL) {
+        next = cur->next; // Save the next node before the callback
+        func(queue, cur->data);
+
+        // Check if the current node has been deleted
+        if (prev == NULL && cur != queue->head) {
+            cur = queue->head;
+        } else if (prev != NULL && prev->next != cur) {
+            cur = prev->next;
+        } else {
+            prev = cur;
+            cur = next;
+        }
+    }
+
+    return 0;
 }
 
-int queue_length(queue_t queue)
-{
-	/* TODO Phase 1 */
+int queue_length(queue_t queue) {
+    if (queue == NULL) {
+        return -1;
+    }
+    return queue->size;
 }
 
