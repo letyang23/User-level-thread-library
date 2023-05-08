ad68d38cfde8c5dbe06fded270c381f807876f92
letyang23 <yangletian0309@gmail.com>
2023-05-08 13:47:46 -0700

    Phase 2 reset

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -10,233 +10,111 @@
 #include "uthread.h"
 #include "queue.h"
 
-enum {
-    STATE_READY, //0
-    STATE_RUNNING, // 1
-    STATE_ZOMBIE, //2
-    STATE_BLOCK//3
-};
+typedef enum {
+    THREAD_RUNNING,
+    THREAD_READY,
+    THREAD_EXITED
+} thread_state_t;
 
-
-struct uthread_tcb {
-    /* TODO Phase 2 */
-    int tid;
-    uthread_ctx_t ctx;
+typedef struct uthread_tcb {
+    ucontext_t context;
+    thread_state_t state;
     void *stack;
-    int state; // 0: ready, 1: running, 2: zombie, 3: blocked
-};
+    struct uthread_tcb *next;
+} uthread_tcb_t;
 
-typedef struct {
-    int count;
-    struct uthread_tcb *main_thread;
-    struct uthread_tcb *running_thread;
-    queue_t ready_queue;
-    queue_t block_queue;
-} Controllor;
+uthread_tcb_t idle_thread;
+static uthread_tcb_t *ready_queue_head = NULL;
+static uthread_tcb_t *ready_queue_tail = NULL;
+static uthread_tcb_t *current_thread = NULL;
 
-Controllor *controllor;
+int uthread_create(uthread_func_t func, void *arg) {
+    uthread_tcb_t *new_thread = (uthread_tcb_t *) malloc(sizeof(uthread_tcb_t));
 
-struct uthread_tcb *uthread_current(void)
-{
-    /* TODO Phase 2/3 */
-    return controllor->running_thread;
-}
-
-int starter(struct uthread_tcb *thread, uthread_func_t func, void *arg) {
-    thread->stack = uthread_ctx_alloc_stack();
-    if (thread->stack == NULL) {
+    if (!new_thread) {
         return -1;
     }
 
-    if (controllor->count == 0) {
-        //Main thread
-        thread->state = STATE_RUNNING;
-        func = NULL;
-        arg = NULL;
+    new_thread->stack = uthread_ctx_alloc_stack();
+
+    if (!new_thread->stack) {
+        free(new_thread);
+        return -1;
+    }
+
+    if (uthread_ctx_init(&new_thread->context, new_thread->stack, func, arg) == -1) {
+        uthread_ctx_destroy_stack(new_thread->stack);
+        free(new_thread);
+        return -1;
+    }
+
+    new_thread->state = THREAD_READY;
+
+    // Add the new thread to the ready queue
+    if (!ready_queue_tail) {
+        ready_queue_head = new_thread;
+        ready_queue_tail = new_thread;
     } else {
-        thread->state = STATE_READY;
+        ready_queue_tail->next = new_thread;
+        ready_queue_tail = new_thread;
     }
+    new_thread->next = NULL;
 
-    if (uthread_ctx_init(&thread->ctx, thread->stack, func, arg) == -1) {
-        uthread_ctx_destroy_stack(thread->stack);
-        free(thread);
-        return -1;
-    }
-
-    thread->tid = controllor->count;
-    controllor->count++;
     return 0;
 }
 
-void uthread_yield(void)
-{
-    /* TODO Phase 2 */
-    preempt_disable();
 
-    struct uthread_tcb *current = controllor->running_thread;
-    if (current->tid != 0) {
-        //Not the main thread
-        controllor->running_thread->state = STATE_READY;
-        if (queue_enqueue(controllor->ready_queue, current) == -1) {
-            //enqueue fail
-            return;
+void uthread_yield(void) {
+    if (ready_queue_head) {
+        uthread_tcb_t *next_thread = ready_queue_head;
+        ready_queue_head = ready_queue_head->next;
+        if (!ready_queue_head) {
+            ready_queue_tail = NULL;
         }
-    }
 
-    if (queue_length(controllor->ready_queue) == 0) {
-        //set main thread as running thread
-        controllor->running_thread = controllor->main_thread;
-        controllor->running_thread->state = STATE_RUNNING;
-    } else {
-        queue_dequeue(controllor->ready_queue, (void**)&controllor->running_thread);
-    }
+        next_thread->state = THREAD_RUNNING;
+        current_thread->state = THREAD_READY;
 
-    preempt_enable();
-    uthread_ctx_switch(&current->ctx, &controllor->running_thread->ctx);
+        if (!ready_queue_head) {
+            ready_queue_head = current_thread;
+            ready_queue_tail = current_thread;
+        } else {
+            ready_queue_tail->next = current_thread;
+            ready_queue_tail = current_thread;
+        }
+        current_thread->next = NULL;
+
+        uthread_ctx_t *prev_context = &current_thread->context;
+        current_thread = next_thread;
+
+        uthread_ctx_switch(prev_context, &current_thread->context);
+    }
 }
 
-void uthread_exit(void)
-{
-    /* TODO Phase 2 */
-    preempt_disable();
 
-    uthread_ctx_t current_context = controllor->running_thread->ctx;
-    controllor->running_thread->state = STATE_ZOMBIE;
-
-    if (queue_length(controllor->ready_queue) == 0) {
-        //set main thread as running thread
-        controllor->running_thread = controllor->main_thread;
-        controllor->running_thread->state = STATE_RUNNING;
-    } else {
-        queue_dequeue(controllor->ready_queue, (void**)&controllor->running_thread);
-    }
-
-    preempt_enable();
-    uthread_ctx_switch(&current_context, &controllor->running_thread->ctx);
-}
-
-int uthread_create(uthread_func_t func, void *arg)
-{
-    /* TODO Phase 2 */
-    struct uthread_tcb *thread = malloc(sizeof(struct uthread_tcb));
-    if (!thread) {
-        return -1;
-    }
-
-    thread->stack = uthread_ctx_alloc_stack();
-    if (thread->stack == NULL) {
-        free(thread);
-        return -1;
-    }
-
-
-    if (controllor->count != 0) {
-        thread->state = STATE_READY;
-    }
-
-    if (uthread_ctx_init(&thread->ctx, thread->stack, func, arg) == -1) {
-        uthread_ctx_destroy_stack(thread->stack);
-        free(thread);
-        return -1;
-    }
-
-    thread->tid = controllor->count;
-    controllor->count++;
-
-    if (queue_enqueue(controllor->ready_queue, (void*)thread) == -1) {
-        return -1;
-    }
-
-    return 0;
-}
-
-int uthread_run(bool preempt, uthread_func_t func, void *arg)
-{
-    /* TODO Phase 2 */
-    if(preempt) {
-        preempt_start(preempt);
-    }
-
-    controllor = (Controllor*)malloc(sizeof(Controllor));
-    controllor->count = 0;
-    controllor->ready_queue = queue_create();
-    controllor->block_queue = queue_create();
-    if (controllor->ready_queue == NULL || controllor->block_queue == NULL) {
-        return -1;
-    }
-
-    struct uthread_tcb *main_thread = malloc(sizeof(struct uthread_tcb));
-    if (!main_thread) {
-        return -1;
-    }
-
-    main_thread->stack = uthread_ctx_alloc_stack();
-    if (main_thread->stack == NULL) {
-        return -1;
-    }
-
-    if (controllor->count == 0) {
-        //Main thread
-        main_thread->state = STATE_RUNNING;
-        func = NULL;
-        arg = NULL;
-    }
-
-    if (uthread_ctx_init(&main_thread->ctx, main_thread->stack, func, arg) == -1) {
-        uthread_ctx_destroy_stack(main_thread->stack);
-        free(main_thread);
-        return -1;
-    }
-
-    main_thread->tid = controllor->count;
-    controllor->count++;
-
-    controllor->main_thread = main_thread;
-    controllor->running_thread = main_thread;
-    uthread_create(func, arg);
+void uthread_exit(void) {
+    current_thread->state = THREAD_EXITED;
     uthread_yield();
+}
+
+int uthread_run(bool preempt, uthread_func_t func, void *arg) {
+    if (uthread_create(func, arg) == -1) {
+        return -1;
+    }
+
+    idle_thread.context.uc_link = NULL;
+    idle_thread.stack = NULL;
+    idle_thread.state = THREAD_RUNNING;
+    current_thread = &idle_thread;
+
+    while (ready_queue_head) {
+        uthread_yield();
+    }
 
     return 0;
 }
 
-void uthread_block(void)
-{
-    preempt_disable();
-    //temporary store the current running thread
-    struct uthread_tcb* savedThread = controllor->running_thread;
-    //if thread is not main thread, put it back to block queue
-    if(savedThread->tid != 0){
-        controllor->running_thread->state = STATE_BLOCK;
-        if(queue_enqueue(controllor->block_queue,
-                         controllor->running_thread) == -1){
-            return;
-        }
-    }
 
-    //if queue is not empty, get thread from the ready else from mainthread
-    if(queue_length(controllor->ready_queue) != 0){
-        //dequeue from the ready queue
-        queue_dequeue(controllor->ready_queue,
-                      (void**)&controllor->running_thread);
-        controllor->running_thread->state = STATE_RUNNING;
-    }else{
-        //set main thread to running thread
-        controllor->running_thread = controllor->main_thread;
-        controllor->running_thread->state = STATE_RUNNING;
-    }
-    preempt_enable();
-    //switch thread
-    uthread_ctx_switch(&savedThread->ctx,
-                       &controllor->running_thread->ctx);
 
-}
 
-void uthread_unblock(struct uthread_tcb *uthread)
-{
 
-    //delete uthread from block queue
-    queue_delete(controllor->block_queue, uthread);
-    //put thread back to the ready queue
-    queue_enqueue(controllor->ready_queue, uthread);
-
-}
